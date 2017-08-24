c66f9b86f8c156bfe129a95d60c9a88e074a4ba8
dbarlavento <dbg@a.recife.ifpe.edu.br>
2017-08-24 20:38:08 -0300

    Mostra todos os registros de calorias coletados pelo relógio.

--- Abrindo um arquivo XML/teste01.js
+++ Abrindo um arquivo XML/teste01.js
@@ -1,10 +1,28 @@
+var xmlDoc;
+var painel;
+var calorias;
+
 function xmlLoader(url) {
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
     		if (this.readyState === 4 && this.status === 200) {
-       			document.getElementById("xml").innerHTML = xhttp.responseText;
+			xmlDoc = xhttp.responseXML;
+       			getCalorias(xmlDoc);
     		}	
 	};
 	xhttp.open("GET", url, true);
 	xhttp.send();
+}
+
+function getCalorias(xml) {
+	var calValor;
+
+	calorias = xml.getElementsByTagName("Calories");
+	painel = document.getElementById("calorias");
+	for(i = 0; i < calorias.length; i++){
+		calValor = document.createElement("p");
+		calValor.innerHTML = 
+		xml.getElementsByTagName("Calories")[i].childNodes[0].nodeValue;
+		painel.appendChild(calValor);
+	}
 }
